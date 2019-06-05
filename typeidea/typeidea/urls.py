474d646f5a53a6e08d1d8ab6e7708e3919679c76
yongxin_centos <leeyongxin@126.com>
2019-06-05 17:35:35 +0800

    middle save

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -24,17 +24,18 @@ from wind import views as wv
 
 urlpatterns = [
     url(r'^$', post_list, name='home'),
-    url(r'^wind/example.html/$', wv.TestHtml.as_view(), name='test'),
     url(r'^category/(?P<category_id>\d+)/$', post_list),
     url(r'^tag/(?P<tag_id>\d+)/$', post_list),
     url(r'^post/(?P<post_id>\d+).htmls/$', post_detail),
     url(r'^links/$', links),
+
+    url(r'^wind/pic/$', wv.getimage, name='wind_pic_test'),
     url(r'^wind/$', wv.DbListView.as_view(), name='wind_home'),
     url(r'^wind/(?P<slug>[a-zA-Z0-9_-]*).html/$', wv.TableListView.as_view(), name='db_view'),
     url(r'^wind/(?P<db>[a-zA-Z0-9_-]*)/(?P<table>[a-zA-Z0-9_-]*).html/$',
             wv.TableDetailView.as_view(), name='table_view'),
     url(r'^wind/(?P<db>[a-zA-Z0-9_-]*)/(?P<table>[a-zA-Z0-9_-]*)/pic/$',
-            wv.getimage, name='pic_view'),
+            wv.ImageView.as_view(), name='pic_view'),
     url('^super_admin/', admin.site.urls),
     url('^admin/', custom_site.urls),
 ]
--- typeidea/wind/views.py
+++ typeidea/wind/views.py
@@ -16,6 +16,16 @@ from .models import Database, DbTable
 from .funlib import Mongo
 from .forms import QueryTimeForm
 import json
+import logging
+import datetime
+
+# logger setting
+lg = logging.getLogger('view')
+ch = logging.StreamHandler()
+formatter_f = logging.Formatter('[%(asctime)s][%(process)d:%(thread)d][%(levelname)s] %(message)s')
+ch.setFormatter(formatter_f)
+lg.setLevel(logging.DEBUG)
+lg.addHandler(ch)
 
 # Create your views here.
 class TestHtml(TemplateView):
@@ -102,23 +112,60 @@ class TableListView(DbListView, ListView):
             if col.col_name not in table_list:
                 col.delete()
 
+class TitleSearchMixin():
+
+    def get_queryset(self,q=None):
+        # fetch queryset from the parent's get_queryset
+        lg.debug("now in {0}".format("TitleSearchMixin.get_queryset"))
+        queryset = super().get_queryset()
+        lg.debug("TitleSearchMixin.queryset =\n {0}".format(queryset))
+
+        # get the q GET parameter
+        v = self.request.GET.get(q)
+        if v:
+            #return a filtered queryset
+            #return queryset.filter(title__iconstains=v)
+            return v
+
+        # no q is specified then we return queryset
+        return queryset
 
 class TableDetailView(ListView):
     template_name = 'wind/home.html'
-    queryset = ''
+    queryset = DbTable.objects.all()
+
 
 
     def get_queryset(self, *args,  **kwargs):
-        print("now in get")
-        qs = super().get_queryset()
-        qstart = self.request.GET.get("start_time")
-        qstop = self.request.GET.get("end_time")
-        qselected = self.request.GET.getlist("check")
-        print(qstart)
-        print(qstop)
-        print(qselected)
-        print(self.request.GET)
-        return qs
+        lg.debug("now in {0}".format("TableDetailView.get_queryset"))
+        if self.request.GET.get('time_btn_clicked'):
+            tstart = self.request.GET.get('start_time').replace("T"," ")
+            lg.debug("start time = {}".format(tstart))
+            tstart = datetime.datetime.strptime(tstart, "%Y-%m-%d %H:%M")
+            lg.debug("start time = {}".format(tstart))
+
+            tstop = self.request.GET.get('end_time').replace("T"," ")
+            tstop = datetime.datetime.strptime(tstop, "%Y-%m-%d %H:%M")
+            lg.debug("TableListView.get_queryset.time_btn_clicked\nstart_time:{0} stop_time:{1}".
+                    format(tstart, tstop)
+
+                    )
+            self.get_data(tstart, tstop)
+
+
+    def get_data(self, start, stop):
+        mon = Mongo()
+        # add talbe if notexist
+        #query1 = "find({'time':{'$gt':{0},'$lt':{1}}, \
+        #                'Power':{'$lt':1700},                           \
+        #                'SubState':{'$eq':30}},                         \
+        #               {'_id':0,'time':1,'WindSpeed':1,'Power':1, 'PitchDemand':1})".format()
+        query = {'TimeStamp':{'$gt':start,'$lt':stop}}
+        lg.debug("query = {}".format(query))
+
+        headlist = pd.DataFrame(list(mon.connect_mongo()[self.kwargs['db']][self.kwargs['table']].find(query).limit(2)))
+        lg.debug("db = {0}\n table = {1}".format([self.kwargs['db']], [self.kwargs['table']]))
+        lg.debug("here is findings:\n {}".format(headlist))
 
 
     def get_context_data(self, **kwargs):
@@ -129,7 +176,7 @@ class TableDetailView(ListView):
             'collections':DbTable.objects.filter(db=self.kwargs['db']),
             'headlist': hlist,
             'model_db': self.kwargs['db'],
-            #'model_table': self.kwargs['table'],
+            'model_table': self.kwargs['table'],
             'dbs': Database.objects.all(),
             'form':QueryTimeForm()
 
@@ -137,24 +184,106 @@ class TableDetailView(ListView):
         return context
 
     def post(self, request, db, table):
-        print("now in post")
-        #print(request.POST)
+        lg.debug("now in {0}".format("TableDetailView.post"))
 
         return render(request, 'wind/base.html', {'db':db, 'table':table})
 
 
-from django.http import HttpResponse
-from django.shortcuts import render
+
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.dates import DateFormatter
 import matplotlib.pyplot as plt
 import random
-import datetime
 import io
 import base64
+import pandas as pd
 
-def getimage(request,db=None, table=None):
+class ImageView(TableDetailView):
+    template_name = 'wind/pic.html'
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.btn_sw = {}
+
+
+    def get_context_data(self, *args, **kwargs):
+        #lg.debug(self.kwargs)
+        context = super().get_context_data(**kwargs)
+        if self.btn_sw.get('draw',None):
+            pic = self.getimage(self.request)['inline_png']
+            context.update({
+                'inline_png':pic
+                })
+        lg.debug("get message:\n{}".format(self.request.GET))
+        return context
+
+#    def get(self, *args, **kwargs):
+#        lg.debug("get message:\n{}".format(self.request.GET))
+#        qstart = self.request.GET.get("start_time")
+#        qstop = self.request.GET.get("end_time")
+#        lg.debug("now in ImageView.get:\n{}{}".format(qstart, qstop))
+#        return render(self.request, 'wind/base.html')
+
+    def get_queryset(self):
+        lg.debug("now in {0}".format("ImageView.get_queryset"))
+        lg("reuqestis:\n{}".format(self.request))
+        if self.request.GET.get('draw_btn_clicked'):
+            lg.debug("ImageView.get_queryset.draw_btn_clicked")
+            self.btn_sw.update({
+                'draw':True
+                })
+        if self.request.GET.get('time_btn_clicked'):
+            tstart = self.request.GET.get('start_time')
+            tstop = self.request.GET.get('end_time')
+            lg.debug("ImageView.get_queryset.time_btn_clicked\nstart_time:{0} stop_time:{1}".
+                    format(tstart, tstop)
+
+                    )
+
+            #self.get_data(tstart, tstop)
+
+#
+
+    def get_data(self, start, stop):
+        mon = Mongo()
+        # add talbe if notexist
+        #query1 = "find({'time':{'$gt':{0},'$lt':{1}}, \
+        #                'Power':{'$lt':1700},                           \
+        #                'SubState':{'$eq':30}},                         \
+        #               {'_id':0,'time':1,'WindSpeed':1,'Power':1, 'PitchDemand':1})".format()
+        query = "find({'time':{'$gt':{0},'$lt':{1}})".format(start, stop)
+
+        headlist = pd.DataFrame(list(mon.connect_mongo()[self.kwargs['db']][self.kwargs['table']].find(eval(query)).limit(2)))
+        lg.debug("here is findings:\n {}".format(headlist))
+
+
+    def getimage(self, request):
+        # Construct the graph
+        fig, ax = plt.subplots()
+        x=[]
+        y=[]
+        now=datetime.datetime.now()
+        delta=datetime.timedelta(days=1)
+        for i in range(10):
+            x.append(now)
+            now+=delta
+            y.append(random.randint(0, 1000))
+        ax.plot_date(x, y, '-')
+        ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+        fig.autofmt_xdate()
+        canvas=FigureCanvasAgg(fig)
+        buf = io.BytesIO()
+        #plt.savefig(buf, format='png')
+        canvas.print_png(buf)
+        #plt.close(fig)
+        ret = {}
+        #ret['inline_png']= base64.b64encode(buf.getvalue()) for python3, need to add decode
+        ret['inline_png']= base64.b64encode(buf.getvalue()).decode()
+        response=HttpResponse(buf.getvalue(),content_type='image/png')
+        return ret
+        #return render(request, 'wind/pic.html', ret)
+
+def getimage(request):
     # Construct the graph
     fig, ax = plt.subplots()
     x=[]
@@ -177,5 +306,5 @@ def getimage(request,db=None, table=None):
     #ret['inline_png']= base64.b64encode(buf.getvalue())
     ret['inline_png']= base64.b64encode(buf.getvalue()).decode()
     response=HttpResponse(buf.getvalue(),content_type='image/png')
-    return render(request, 'wind/detail.html', ret)
-    #return response
+    #return ret
+    return render(request, 'wind/pic.html', ret)
